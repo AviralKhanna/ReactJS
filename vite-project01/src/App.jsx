9de8cecbffdcf55e5fe300a4d3f52b72de2a67cb
AviralKhanna <aviral.projects24@gmail.com>
2024-12-24 02:28:55 +0530

    react

--- vite-project01/src/App.jsx
+++ vite-project01/src/App.jsx
@@ -1,28 +1,33 @@
-// import { useState } from "react";
-// import "./App.css";
+import { useState } from "react";
+import "./App.css";
 
-// function App() {
-//   const [counter, setCounter] = useState(0);
+function App() {
 
-//   const addValue = () => {
-//     if(counter<20){              // value not above 20
-//     setCounter(counter + 1);}
-//   };
+  return (
+    <>
+  <h1 className="bg-cyan-400 text-black p-4 rounded-xl">Tailwind Introduction!</h1>
+  <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" width="384" height="512" />
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. Its easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        {username}
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        "location"
+      </div>
+    </figcaption>
+  </div>
+</figure>
+    </>
+  );
+}
 
-//   const removeValue = () => {
-//     if(counter>0)               // no negative value.
-//     setCounter(counter - 1);
-//   };
-
-//   return (
-//     <>
-//       <h1>Hello, Project Counter through APP</h1>
-//       <h3>Counter Add and Removal</h3>
-//       <p>Current Counter Value: {counter}</p>
-//       <button onClick={addValue}>Increase Counter {counter}</button>
-//       <button onClick={removeValue}>Decrease Counter {counter}</button>
-//     </>
-//   );
-// }
-
-// export default App;
+export default App;
--- vite-project01/src/main.jsx
+++ vite-project01/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from './App.jsx'
+import App from './App.jsx'
 import Counter from "./Counter01.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Counter />
+    <App username="Aviral"/>
   </StrictMode>
 );
