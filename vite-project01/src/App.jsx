b1522514d74da5a306006a127599ab900db42d42
AviralKhanna <aviral.projects24@gmail.com>
2024-12-24 01:11:13 +0530

    react hooks

---
+++ vite-project01/src/App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   const addValue = () => {
+//     if(counter<20){              // value not above 20
+//     setCounter(counter + 1);}
+//   };
+
+//   const removeValue = () => {
+//     if(counter>0)               // no negative value.
+//     setCounter(counter - 1);
+//   };
+
+//   return (
+//     <>
+//       <h1>Hello, Project Counter through APP</h1>
+//       <h3>Counter Add and Removal</h3>
+//       <p>Current Counter Value: {counter}</p>
+//       <button onClick={addValue}>Increase Counter {counter}</button>
+//       <button onClick={removeValue}>Decrease Counter {counter}</button>
+//     </>
+//   );
+// }
+
+// export default App;
---
+++ vite-project01/src/Counter01.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function Counter01() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    if (counter < 20) {
+      // value not above 20
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0)
+      // no negative value.
+      setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>Hello, Project Counter through new file</h1>
+      <h3>Counter Add and Removal</h3>
+      <p>Current Counter Value: {counter}</p>
+      <button onClick={addValue}>Increase Counter {counter}</button>
+      <button onClick={removeValue}>Decrease Counter {counter}</button>
+    </>
+  );
+}
+
+export default Counter01;
