a84e12fd1f47c285200fba545be87bff7a9a7ed0
AviralKhanna <aviral.projects24@gmail.com>
2025-01-14 18:03:12 +0530

    decreased

--- 03CurrencyConvertor/src/App.jsx
+++ 03CurrencyConvertor/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+/* CUSTOM HOOKS
+why? -> when we need to fetch API's we need to have custom hooks to handle them.
+custom hooks help us return the desired data from the API we have called.
+
+*/
+
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
