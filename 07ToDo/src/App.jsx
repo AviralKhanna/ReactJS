f8f01f2d6509c5cfb6b4bd5b15e49c045f8c01a1
AviralKhanna <aviral.projects24@gmail.com>
2025-01-17 09:59:05 +0530

    07 todo

---
+++ 07ToDo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-blue-700 text-orange-300 px-3 py-3'>hello project 07. thala for no reason!</h1>
+    </>
+  )
+}
+
+export default App
